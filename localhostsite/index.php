632b3b6ba6e961678631003bfd07efe198c0ae8a
Большаков Сергей <bolshakov.s@madin.ru>
2022-11-06 20:54:27 +0300

    update readme

--- localhostsite/index.php
+++ localhostsite/index.php
@@ -1,24 +1,33 @@
 <?php
 //phpinfo();
+$first_name = null;
+$last_name = null;
+$middle_name = null;
+
+if(isset($_POST["firstname"])){$first_name = strip_tags($_POST["firstname"]);}
+if(isset($_POST["lastname"])){$last_name = strip_tags($_POST["lastname"]);}
+if(isset($_POST["middlename"])){$middle_name = strip_tags($_POST["middlename"]);}
+
+echo "Имя: $first_name <br> Фамилия: $last_name";
 if( isset( $_POST['debug'] ) )
 {
     /*
     echo '<pre>';
         print_r($_COOKIE);
     echo '</pre>';
-    
+
     echo '<pre>';
         print_r($_ENV);
     echo '</pre>';
-    
+
     echo '<pre>';
         print_r($_FILES);
     echo '</pre>';
-    
+
     echo '<pre>';
         print_r($_GET);
     echo '</pre>';
-    
+
     echo '<pre>';
         print_r($_POST);
     echo '</pre>';
@@ -26,11 +35,11 @@ if( isset( $_POST['debug'] ) )
     echo '<pre>';
         print_r($_REQUEST);
     echo '</pre>';
-    /*
+    
     echo '<pre>';
         print_r($_SERVER);
     echo '</pre>';
-    
+    /*
     echo '<pre>';
         print_r($_SESSION);
     echo '</pre>';
