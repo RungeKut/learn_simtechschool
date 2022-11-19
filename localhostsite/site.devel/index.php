9a4acf3fcd237b4f36539c2803bcdfd8138089ad
Большаков Сергей <bolshakov.s@madin.ru>
2022-11-19 20:17:37 +0300

    add pagination

--- localhostsite/site.devel/index.php
+++ localhostsite/site.devel/index.php
@@ -1,6 +1,4 @@
 <?php
-include 'output.php';
-//phpinfo();
 $photo_input = null;
 $firstname_input = null;
 $lastname_input = null;
@@ -12,8 +10,6 @@ $gender_input = null;
 $advertising_input = null;
 $message_input = null;
 $country_input = null;
-$maxRowsOut = 10;
-$activePage = 0;
 $customerReviews = null;
 $servername = "localhost";
 $username = "root";
@@ -21,51 +17,41 @@ $db_password = "root";
 $db = 'feedbackForm';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(isset($_POST["photo"])){$photo_input = strip_tags($_POST["photo"]);}
+    if(isset($_POST["firstname"])){$firstname_input = strip_tags($_POST["firstname"]);}
+    if(isset($_POST["lastname"])){$lastname_input = strip_tags($_POST["lastname"]);}
+    if(isset($_POST["middlename"])){$middlename_input = strip_tags($_POST["middlename"]);}
+    if(isset($_POST["email"])){$email_input = strip_tags($_POST["email"]);}
+    if(isset($_POST["birthday"])){$birthday_input = strip_tags($_POST["birthday"]);}
+    if(isset($_POST["tel"])){$tel_input = strip_tags($_POST["tel"]);}
+    if(isset($_POST["gender"])){$gender_input = strip_tags($_POST["gender"]);}
+    if(isset($_POST["advertising"])){$advertising_input = strip_tags($_POST["advertising"]);}
+    if(isset($_POST["message"])){$message_input = strip_tags($_POST["message"]);}
+    if(isset($_POST["country"])){$country_input = strip_tags($_POST["country"]);}
 
-if(isset($_POST["photo"])){$photo_input = strip_tags($_POST["photo"]);}
-if(isset($_POST["firstname"])){$firstname_input = strip_tags($_POST["firstname"]);}
-if(isset($_POST["lastname"])){$lastname_input = strip_tags($_POST["lastname"]);}
-if(isset($_POST["middlename"])){$middlename_input = strip_tags($_POST["middlename"]);}
-if(isset($_POST["email"])){$email_input = strip_tags($_POST["email"]);}
-if(isset($_POST["birthday"])){$birthday_input = strip_tags($_POST["birthday"]);}
-if(isset($_POST["tel"])){$tel_input = strip_tags($_POST["tel"]);}
-if(isset($_POST["gender"])){$gender_input = strip_tags($_POST["gender"]);}
-if(isset($_POST["advertising"])){$advertising_input = strip_tags($_POST["advertising"]);}
-if(isset($_POST["message"])){$message_input = strip_tags($_POST["message"]);}
-if(isset($_POST["country"])){$country_input = strip_tags($_POST["country"]);}
+    $mysqli = mysqli_connect($servername, $username, $db_password, $db);
 
-$mysqli = mysqli_connect($servername, $username, $db_password, $db);
-
-if (!$mysqli) {
-    die("Connection to DB is failed!" . mysqli_connect_error());
-} else {
-    try {
-        $inputsIsWrited = $mysqli->query( query: "INSERT INTO customer_reviews (photo, firstname, lastname, middlename, email, birthday, tel, gender, advertising, message, country) VALUES ('$photo_input','$firstname_input','$lastname_input','$middlename_input','$email_input','$birthday_input','$tel_input','$gender_input','$advertising_input','$message_input','$country_input')");
-        /*
-        $reviews = $mysqli->query( query: "SELECT * FROM customer_reviews");
-        while($result = mysqli_fetch_array($reviews, mode: MYSQLI_ASSOC)) {
-            $customerReviews[] = $result;
+    if (!$mysqli) {
+        die("Connection to DB is failed!" . mysqli_connect_error());
+    } else {
+        try {
+            $inputsIsWrited = $mysqli->query( query: "INSERT INTO customer_reviews (photo, firstname, lastname, middlename, email, birthday, tel, gender, advertising, message, country) VALUES ('$photo_input','$firstname_input','$lastname_input','$middlename_input','$email_input','$birthday_input','$tel_input','$gender_input','$advertising_input','$message_input','$country_input')");
+        } catch (Exception $e) {
+            echo '<div class="alert alert-warning" role="alert">
+            <?php echo $e->getMessage(); ?>
+            </div>';
         }
-        */
-        /*
-        echo '<pre>';
-        print_r($customerReviews);
-        echo '</pre>';
-        */
-    } catch (Exception $e) {}
     }
-} else {
-    $reviews = $mysqli->query( query: "SELECT * FROM customer_reviews");
-        while($result = mysqli_fetch_array($reviews, mode: MYSQLI_ASSOC)) {
-            $customerReviews[] = $result;
-        }
-        echo '<pre>';
-        print_r($customerReviews);
-        echo '</pre>';
+    if (isset($inputsIsWrited)) {
+        echo '<div class="alert alert-success" role="alert">
+              Ваш отзыв принят. Благодарим за сотрудничество!
+              </div>';
+    } else {
+        echo '<div class="alert alert-danger" role="alert">
+              Ошибка! Ваш отзыв не записан. Пожалуйста повторите еще раз.
+              </div>';
+    }
 }
-
-    
-
 require 'index.html';
 exit;
 ?>
--- localhostsite/site.devel/output.php
+++ localhostsite/site.devel/output.php
@@ -1,4 +1,37 @@
 <?php
-//echo 'fghgf';
-
+    $servername = "localhost";
+    $username = "root";
+    $db_password = "root";
+    $db = 'feedbackForm';
+    $link = mysqli_connect($servername, $username, $db_password, $db);
+    $activePage = 1;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST["turnPages"])) {
+            if(isset($_POST["turnPages"])){$maxRowsOut = strip_tags($_POST["turnPages"]);}
+        }
+    } else {
+        $maxRowsOut = 3;
+    }
+    if (!$link) {
+        die("Connection to DB is failed!" . mysqli_connect_error());
+    } else {
+        $result = mysqli_query($link, "SELECT * FROM customer_reviews");
+        $total_records = mysqli_num_rows($result);
+    }
+    $pages = ceil($total_records/$maxRowsOut);
+    if (isset($_GET["page"])) {
+        $inputPage = $_GET["page"];
+        if (($inputPage > 0) & ($inputPage <= $pages)) {
+            $activePage = $inputPage;
+        }
+    }
+    $start_from = ($activePage - 1) * $maxRowsOut;
+    
+    if (!$link) {
+        die("Connection to DB is failed!" . mysqli_connect_error());
+    } else {
+        $outData = $link->query( query: "SELECT * FROM customer_reviews LIMIT $start_from, $maxRowsOut");
+    }
+require 'output.html';
+exit;
 ?>
